df8575b58efc5171cfa46dd882bf35b3c145218c
Blaze <fraudmailers@gmail.com>
2026-08-10 16:46:28 +0200

    Engine.h

---
+++ Engine.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <windows.h>
+
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+#define STATUS_UNSUCCESSFUL ((NTSTATUS)0xC0000001L)
+#define STATUS_INFO_LENGTH_MISMATCH ((NTSTATUS)0xC0000004L)
+#define STATUS_BUFFER_TOO_SMALL ((NTSTATUS)0xC0000023L)
+
+// Raw Assembly Linker Declarations
+extern "C" NTSTATUS DirectSyscallBridge(DWORD ssn, ...);
+
+// Hidden Internal Windows Structures for Process Enumeration
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG NumberOfThreads;
+    LARGE_INTEGER WorkingSetPrivateSize;
+    ULONG HardFaultCount;
+    ULONG NumberOfThreadsHighWatermark;
+    ULONGLONG CycleTime;
+    LARGE_INTEGER CreateTime;
+    LARGE_INTEGER UserTime;
+    LARGE_INTEGER KernelTime;
+    UNICODE_STRING ImageName;
+    LONG BasePriority;
+    HANDLE UniqueProcessId;
+    HANDLE InheritedFromUniqueProcessId;
+    ULONG HandleCount;
+    ULONG SessionId;
+    ULONG_PTR UniqueProcessKey;
+    ULONG_PTR PeakVirtualSize;
+    ULONG_PTR VirtualSize;
+    ULONG PageFaultCount;
+    ULONG_PTR PeakWorkingSetSize;
+    ULONG_PTR WorkingSetSize;
+    ULONG_PTR QuotaPeakPagedPoolUsage;
+    ULONG_PTR QuotaPagedPoolUsage;
+    ULONG_PTR QuotaPeakNonPagedPoolUsage;
+    ULONG_PTR QuotaNonPagedPoolUsage;
+    ULONG_PTR PagefileUsage;
+    ULONG_PTR PeakPagefileUsage;
+    ULONG_PTR PrivatePageCount;
+    LARGE_INTEGER ReadOperationCount;
+    LARGE_INTEGER WriteOperationCount;
+    LARGE_INTEGER OtherOperationCount;
+    LARGE_INTEGER ReadTransferCount;
+    LARGE_INTEGER WriteTransferCount;
+    LARGE_INTEGER OtherTransferCount;
+} SYSTEM_PROCESS_INFORMATION, *PSYSTEM_PROCESS_INFORMATION;
+
+// High-Performance Multi-Language Export Declarations
+extern "C" {
+    __declspec(dllexport) BOOL __stdcall InvokeNativeAllocation(DWORD processId, DWORD_PTR* outAddress, SIZE_T size, DWORD protect);
+    __declspec(dllexport) BOOL __stdcall InvokeNativeMemoryWrite(DWORD processId, DWORD_PTR targetAddress, PVOID localBuffer, SIZE_T size, SIZE_T* bytesWritten);
+    __declspec(dllexport) BOOL __stdcall InvokeNativeMemoryRead(DWORD processId, DWORD_PTR targetAddress, PVOID localBuffer, SIZE_T size, SIZE_T* bytesRead);
+    __declspec(dllexport) DWORD __stdcall FindTargetProcessId(const wchar_t* processName);
+}
